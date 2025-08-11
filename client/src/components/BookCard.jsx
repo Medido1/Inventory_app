f4ab45f299600ed300d45bccb015239cdb7d9a4c
Medido1 <medi1222@gmail.com>
2025-08-11 11:46:14 +0100

    Add delete button to delete a book from the list

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,33 +1,60 @@
 import { Link } from "react-router-dom";
 import emptyCoverIcon from '../assets/question-sign.png';
-
+import { GlobalContext } from "../context/GlobalContext";
+import { useContext } from "react";
 
 function BookCard({book}) {
+  const {booksData, setBooksData} = useContext(GlobalContext)
+
+  function DeleteBook(id) {
+    if (
+      window.confirm("Are you sure you want to delete this book ?")
+    ) {
+      const filterdBooks = booksData.filter((book) => book.id !== id)
+      setBooksData(filterdBooks)
+    }
+  }
+
   return (
-    <div className="bg-slate-100 p-4 flex mb-4 rounded-md">
-      {!book.cover ? 
-        <div className="border-2 border-dashed p-4 flex flex-col
-          items-center justify-center max-w-[40%] text-center h-[180px]">
-          <img 
-            className="h-10"
-            src={emptyCoverIcon} alt="no cover" />
-          <p>No Cover provided ! </p>
+    <div className="bg-slate-100 p-4 mb-4 rounded-md">
+      <div className="flex">
+        {!book.cover ?
+          <div className="border-2 border-dashed p-4 flex flex-col
+            items-center justify-center max-w-[40%] text-center h-[180px]">
+            <img
+              className="h-10"
+              src={emptyCoverIcon} alt="no cover" />
+            <p>No Cover provided ! </p>
+          </div>
+          :
+          <img className="h-[160px]" src={book.cover} alt={`${book.title} cover`}/>
+        }
+        <div className="ml-2 text-center flex-1">
+          <h2 className="text-2xl">{book.title}</h2>
+          <p>{book.author}</p>
+          <ul className="text-blue-400 flex flex-col">
+            {book.categories.map((category) =>
+              <Link
+                to={`/categories/${category}`}
+                key={category}>
+                {category}
+              </Link>
+            )}
+          </ul>
         </div>
-        :
-        <img className="h-[160px]" src={book.cover} alt={`${book.title} cover`}/>
-      } 
-      <div className="ml-2 text-center flex-1">
-        <h2 className="text-2xl">{book.title}</h2>
-        <p>{book.author}</p>
-        <ul className="text-blue-400 flex flex-col">
-          {book.categories.map((category) =>
-            <Link 
-              to={`/categories/${category}`}
-              key={category}>
-              {category}
-            </Link>
-          )}
-        </ul>
+      </div>
+      <div className="flex justify-center gap-4 mt-4">
+        <button 
+        
+          className="bg-green-200 px-4 py-2 rounded-lg cursor-pointer">
+          Edit
+        </button>
+        <button
+          onClick={() => DeleteBook(book.id)}
+          className="bg-red-200 px-4 py-2 rounded-lg cursor-pointer"
+        >
+          Delete
+        </button>
       </div>
 
     </div>
