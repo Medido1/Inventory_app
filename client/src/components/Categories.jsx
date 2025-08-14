b907b69b458ffefa92a620f0a6e9d18cadef9fc3
Medido1 <medi1222@gmail.com>
2025-08-14 13:41:36 +0100

    Refractor Categories componenet

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -18,26 +18,30 @@ function Categories() {
       }) 
     })
     setCategoriesList(uniqueCategories)
-  }, [])
+  }, [booksData])
 
   return (
     <div className='p-4'>
       <div className='flex items-center gap-4 mb-4'>
-        <img src={categoriesIcon} alt="icon" className='h-10' />
+        <img src={categoriesIcon} alt="icon" className='h-10' aria-hidden="true" />
         <h1 className="text-2xl text-center">Categories</h1>
       </div>
-      <ul className='flex flex-col sm:grid sm:grid-cols-2 sm:gap-2'>
-        {categoriesList.map((category) => 
-          <Link 
-            to={`/categories/${category}`}
-            key={category}
-            className='bg-slate-200 p-4 text-black rounded-md text-center text-xl mb-4
-              font-bold cursor-pointer'
-          >
-            {category}
-          </Link>
-        )}
-      </ul>
+      <div className='flex flex-col sm:grid sm:grid-cols-2 sm:gap-2'>
+        {categoriesList.length > 0 ? 
+          (categoriesList.map((category) => 
+            <Link 
+              to={`/categories/${category}`}
+              key={category}
+              className='bg-slate-200 p-4 text-black rounded-md text-center text-xl mb-4
+                font-bold cursor-pointer'
+            >
+              {category}
+            </Link>
+          ))
+          :
+          (<p className='text-center'>No Categories yet :(</p>)
+        }
+      </div>
     </div>
   )
 }
