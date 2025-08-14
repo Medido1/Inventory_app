2e2c6ed7174f4b26d6fa62ea81a84713a57f3716
Medido1 <medi1222@gmail.com>
2025-08-14 12:04:28 +0100

    Refractor header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,17 +3,22 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalContext';
 import NavMenuDesk from './NavMenuDesk';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  const {isMobile, setIsMenuOpen} = useContext(GlobalContext)
+  const {isMobile, setIsMenuOpen, isMenuOpen} = useContext(GlobalContext)
   return (
     <header className='header p-6 flex justify-between items-center'>
-      <img 
-        className='h-14'
-        src={logo} alt="logo" 
-      />
+      <Link to="/">
+        <img 
+          className='h-14'
+          src={logo} alt="My Books - Return to homepage"
+        />
+      </Link>
       {isMobile && 
         <button 
+          aria-label="Open menu"
+          aria-expanded= {!!isMenuOpen} // forces value to be boolean
           onClick={() => setIsMenuOpen(prev => !prev)}>
           <RxHamburgerMenu style={{ color: "white", fontSize: "30px" }}/>
         </button>
