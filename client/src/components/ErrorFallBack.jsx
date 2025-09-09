51f9084191f2a8cb9fc235dbcde376be5efc3dda
Medido1 <medi1222@gmail.com>
2025-09-09 10:50:33 +0100

    Refractor ErrorFallBack

--- client/src/components/ErrorFallBack.jsx
+++ client/src/components/ErrorFallBack.jsx
@@ -1,8 +1,8 @@
-function ErrorFallback({ error, resetErrorBoundary }) {
+function ErrorFallback({resetErrorBoundary }) {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
+      {/* reset the error state so the component tree that crashed can try rendering again. */}
       <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   );
